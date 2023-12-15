362510b5916f64d5ed8eed5bd99fc893d084e58e
Essam Ashraf <145169567+Essam3Ashraf@users.noreply.github.com>
2023-12-15 22:19:13 +0200

    TWI Additions
    TWI Additions

---
+++ COTS/MCAL/MCAL_TWI/MCAL_TWI_Config.h
@@ -0,0 +1,14 @@
+/*
+ * MCAL_I2C_Config.h
+ *
+ *  Created on: Dec 13, 2023
+ *      Author: 20155
+ */
+
+#ifndef MCAL_MCAL_TWI_MCAL_TWI_CONFIG_H_
+#define MCAL_MCAL_TWI_MCAL_TWI_CONFIG_H_
+
+#define Bit_Rate 12
+#define Prescaler_Value _1_     /*{_1_,_4_,_16_,_64_}*/
+
+#endif /* MCAL_MCAL_TWI_MCAL_TWI_CONFIG_H_ */
